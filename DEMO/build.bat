293b3b790ba93ac6930fb39df2da4e5f1e9ac09c
fdescamp <florence.descamps@intersystems.com>
2016-08-23 14:28:43 -0700

    Add build.bat for continuous integration

---
+++ DEMO/build.bat
@@ -0,0 +1,45 @@
+:::::::::::::::::::::::
+:: BUILD BATCH FILE  ::
+:::::::::::::::::::::::
+::
+:: PATH TO CACHE DIRECTORY AND EXECUTABLES
+set CACHEDIR=C:\InterSystems\HS20164QA
+set CACHEBIN=%CACHEDIR%\bin\cache
+set CACHEINSTANCE=HS20164QA
+
+:: Check build variables
+IF NOT DEFINED WORKSPACE EXIT 1
+IF NOT DEFINED JOB_NAME EXIT 1
+set SRCDIR=%WORKSPACE%
+
+:: PREPARE OUTPUT FILE
+set OUTFILE=%CD%\outFile
+del "%OUTFILE%"
+
+
+:: NOW, PREPARE TO CALL CACHE
+::
+:: FIRST, LOAD BUILD CLASS TO USER NAMESPACE
+echo set sc=$SYSTEM.OBJ.Load("%SRCDIR%\DEMO\Util.Build.cls","ck") >inFile
+
+:: IF UNSUCCESSFULL, DISPLAY ERROR
+echo if sc'=1 do $SYSTEM.OBJ.DisplayError(sc) >>inFile
+
+:: NOW, PERFORM BUILD
+echo if sc=1 set sc=##class(Util.Build).Build("%JOB_NAME%","%SRCDIR%") >>inFile
+
+:: IF UNSUCCESSFULL, DISPLAY ERROR
+echo if sc'=1 do $SYSTEM.OBJ.DisplayError(sc) >>inFile
+
+:: IF UNSUCCESSFULL, CREATE OUTPUT FLAG FILE
+echo if sc'=1 set fileName="%OUTFILE%" o fileName:("NWS") u fileName do $SYSTEM.OBJ.DisplayError(sc) c fileName >>inFile
+
+:: THAT'S IT
+echo halt >>inFile
+
+:: CALL CACHE
+
+%CACHEBIN% -s %CACHEDIR%\mgr -U USER <inFile
+
+:: TEST IF THERE WAS AN ERROR
+IF EXIST "%OUTFILE%" EXIT 1
