c7ebb3d1ed01de08b3032934dd71720ffd806e25
fdescamp <florence.descamps@intersystems.com>
2016-08-31 11:12:57 -0700

    ad dmath

---
+++ Tutorial/build.bat
@@ -0,0 +1,47 @@
+:::::::::::::::::::::::
+:: BUILD BATCH FILE  ::
+:::::::::::::::::::::::
+::
+:: PATH TO CACHE DIRECTORY AND EXECUTABLES
+set CACHEDIR=C:\InterSystems\HS20164DEV1
+set CACHEBIN=%CACHEDIR%\bin\cache
+set CACHEINSTANCE=HS20164DEV1
+
+:: Check build variables
+IF NOT DEFINED WORKSPACE EXIT 1
+IF NOT DEFINED JOB_NAME EXIT 1
+set SRCDIR=%WORKSPACE%
+
+:: PREPARE OUTPUT FILE
+set OUTFILE=%CD%\outFile
+del "%OUTFILE%"
+
+
+:: NOW, PREPARE TO CALL CACHE
+:: FIRST, LOAD BUILD CLASS TO USER NAMESPACE
+echo set sc=$SYSTEM.OBJ.Load("%SRCDIR%\Tutorial\Util.Installer.cls","ck") >inFile
+
+:: IF UNSUCCESSFULL, DISPLAY ERROR
+echo if sc'=1 do $SYSTEM.OBJ.DisplayError(sc) >>inFile
+
+:: NOW, PERFORM BUILD
+:: JOB_NAME will be used to build the new Namespace JOB_NAME-BUILD to validate
+:: Add project to test
+set PROJECT="Tutorial"
+echo if sc=1 set sc=##class(Util.Build).Build("%JOB_NAME%","%SRCDIR%","%PROJECT%") >>inFile
+
+:: IF UNSUCCESSFULL, DISPLAY ERROR
+echo if sc'=1 do $SYSTEM.OBJ.DisplayError(sc) >>inFile
+
+:: IF UNSUCCESSFULL, CREATE OUTPUT FLAG FILE
+echo if sc'=1 set fileName="%OUTFILE%" o fileName:("NWS") u fileName do $SYSTEM.OBJ.DisplayError(sc) c fileName >>inFile
+
+:: THAT'S IT
+echo halt >>inFile
+
+:: CALL CACHE
+
+%CACHEBIN% -s %CACHEDIR%\mgr -U USER <inFile
+
+:: TEST IF THERE WAS AN ERROR
+IF EXIST "%OUTFILE%" EXIT 1
